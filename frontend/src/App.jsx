602d613eb9f45608801e8e04212d67b7ef8994e3
suhana <suhanakaura01@gmail.com>
2024-12-17 00:17:31 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home"
 import "./App.css";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Route path='/Register' element={<Register/>}></Route>
       <Route path='/Login' element={<Login/>}></Route>
       <Route path='/ForgotPassword' element={<ForgotPassword/>}></Route>
+      <Route path='/ResetPassword/:token' element={<ResetPassword/>}></Route>
   </Routes>
   )
 }
